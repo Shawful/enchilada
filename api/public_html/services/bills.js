48c075948007e0bfb55fd3590f1559fbecbed3b1
revanthreddy <revanth.reddy@gmail.com>
2014-10-13 10:33:22 -0500

    fixed login on mongo side

--- api/public_html/services/bills.js
+++ api/public_html/services/bills.js
@@ -35,3 +35,39 @@ exports.searchBills = function(MongoClient) {
 
     };
 };
+
+exports.getBillSummary = function() {
+    return function(req, res) {
+    var billId = req.param("billId");    
+    
+    if(! billId)
+        return res.status(400).send('missing bill Id');
+    var limit = req.query.per_page ;
+    if(!limit)
+        limit = 5;
+    var options = {
+                host: 'congress.api.sunlightfoundation.com',
+                path: '/bills/search?bill_id='+billId+'&fields=summary_short',
+                method: 'GET',
+                headers: {'x-apikey': apikey }
+    };
+    
+    var bill = http.request(options, function( response) {
+            
+                response.on('data', function (data) {
+                        data = JSON.parse(data);
+                        
+                        return res.status(200).send(data.results);
+                });
+                response.on('error', function (e) {
+                        console.log(e);
+                        return  res.status(400).send(e);
+                });
+    });
+                        
+    bill.write("");
+    bill.end();
+
+    };
+};
+

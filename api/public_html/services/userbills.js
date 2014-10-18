8b1b7301d1454726cbae507f9ce5b4f2b4353793
revanthreddy <revanth.reddy@gmail.com>
2014-10-18 17:02:19 -0500

    added console stmts

--- api/public_html/services/userbills.js
+++ api/public_html/services/userbills.js
@@ -348,11 +348,10 @@ exports.voteOnABillExperimentAsync = function() {
 
                                             if (data.results[0].voters[senator.id]) {
                                                 
-                                                if ((vote === true && data.results[0].voters[senator.id].vote !== "Yea") ||
+                                                if ((vote === true && data.results[0].voters[senator.id].vote === "Nay") ||
                                                     (vote === false && data.results[0].voters[senator.id].vote === "Yea")
                                                     ) {  // recording disagreement
-                                                      console.log(data.results[0].voters[senator.id].vote);      
-                                                    collection.update({_id: user._id, "senators.id": senator.id}, {$inc: {"senators.$.disagree": 1}}, function(err, records) {
+                                                       collection.update({_id: user._id, "senators.id": senator.id}, {$inc: {"senators.$.disagree": 1}}, function(err, records) {
                                                         if (err) {
                                                             console.log("Disagreement failed with  " + err);
                                                         } else {

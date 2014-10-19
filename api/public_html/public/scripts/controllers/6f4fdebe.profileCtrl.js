98a54967bde20f8c0b4b2d5862e86bc044fe7ef5
Shawn Shaw <shawn.e.shaw@gmail.com>
2014-10-19 10:45:48 -0500

    started profile page scaffold

---
+++ api/public_html/public/scripts/controllers/6f4fdebe.profileCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('politicheckApp')
+    .controller('profileCtrl', ['$rootScope', '$scope', '$http', 'alertService', '$filter', '$timeout', 'Search',
+        function($rootScope, $scope, $http, alertService, $filter, $timeout, Search) {
+
+            
+        }
+    ]);

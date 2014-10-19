373e0f53eea9a449c8b8f88de54ecefc91204a77
Shawn Shaw <shawn.e.shaw@gmail.com>
2014-10-19 15:54:01 -0500

    trying out a blue ribbon

---
+++ api/public_html/public/scripts/controllers/394243a3.historyCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.controller('historyCtrl', [
+    '$scope', '$http', '$rootScope',
+    '$location', 'alertService',
+    function($scope, $http, $rootScope, $location, alertService) {
+
+    }
+]);
